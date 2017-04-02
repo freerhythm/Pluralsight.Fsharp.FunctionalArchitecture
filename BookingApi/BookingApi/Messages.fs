b664ec5c5bba83ce9927feec1ad2a69fe319dc58
Max Kritzinger <max.kritzinger@gmail.com>
2017-04-02 17:25:09 +0100

    * Included Reactive Extensions

---
+++ BookingApi/BookingApi/Messages.fs
@@ -0,0 +1,25 @@
+﻿namespace Pluralsight.Fsharp.BookingApi.HttpApi
+
+open System
+
+type MakeReservation = {
+    Date : DateTime
+    Name : string
+    Email : string
+    Quantity : int }
+
+[<AutoOpen>]
+module Envelope = 
+    [<CLIMutable>]
+    type Envelope<'T> = {
+        Id : Guid
+        Created : DateTimeOffset
+        Item : 'T }
+
+    let Envelop id created item = {
+        Id = id
+        Created = created
+        Item = item }
+
+    let EnvelopWithDefaults item =
+        Envelop (Guid.NewGuid()) (DateTimeOffset.Now) item
