df95d1a8a704e7886094945b1b7c1b2e751e6faa
Max Kritzinger <max.kritzinger@gmail.com>
2017-04-02 16:03:58 +0100

    * Added Renditions

---
+++ BookingApi/BookingApi/Controllers.fs
@@ -0,0 +1,15 @@
+﻿namespace Pluralsight.Fsharp.BookingApi.HttpApi
+
+open System.Net
+open System.Net.Http
+open System.Web.Http
+open Pluralsight.Fsharp.BookingApi.Renditions
+
+type HomeController() =
+    inherit ApiController()
+    member this.Get() = new HttpResponseMessage()
+
+type ReservationController() =
+    inherit ApiController()
+    member this.Post (rendition : MakeReservationRendition ) = 
+        new HttpResponseMessage(HttpStatusCode.Accepted)
---
+++ BookingApi/BookingApi/Renditions.fs
@@ -0,0 +1,8 @@
+﻿namespace Pluralsight.Fsharp.BookingApi.Renditions
+
+[<CLIMutable>]
+type public MakeReservationRendition = {
+    Date : string
+    Name : string
+    Email : string
+    Quantity : int } 
